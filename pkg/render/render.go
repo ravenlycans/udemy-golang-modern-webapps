8045fdd1089c18ee371eeca91dfe033d8da41fda
WolfyLycanDK <raven@lycans.dk>
2024-12-11 10:38:41 +0100

    End of lecture 31

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,67 +1,72 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 // RenderTemplate is a function that renders a template.
-func RenderTemplateTest(w http.ResponseWriter, name string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+name, "./templates/base.layout.tmpl")
-	err := parsedTemplate.Execute(w, nil)
+func RenderTemplate(w http.ResponseWriter, name string) {
+	// Create a template cache
+	tc, err := createTemplateCache()
 	if err != nil {
-		fmt.Printf("renderTemplate: %s\n", err)
-		return
+		log.Fatalf("RenderTemplate: %s", err.Error())
+	}
+
+	// Get the requested template from cache.
+	t, ok := tc[name]
+	if !ok {
+		log.Fatalf("RenderTemplate: %s", err.Error())
+	}
+
+	buf := new(bytes.Buffer)
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Printf("RenderTemplate: %s", err.Error())
+	}
+
+	// Render the template.
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Printf("RenderTemplate: %s", err.Error())
 	}
 }
 
-var tc = make(map[string]*template.Template)
+// createTemplateCache is a function that runs through the ./templates folder, and creates a cache from it.
+func createTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
 
-// RenderTemplate is a function that renders a template from the cache.
-func RenderTemplate(w http.ResponseWriter, t string) {
-	var tmpl *template.Template
-	var err error
+	// get all the files named *.page.tmpl from the ./templates
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
 
-	// check to see if we already have the template in our cache.
-	_, inMap := tc[t]
-	if !inMap {
-		// need to compile and create the template.
-		log.Println("RenderTemplate: Creating new template and adding to cache")
-		err = createTemplateCache(t)
+	// range through all files ending with *.page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
 		if err != nil {
-			log.Printf("RenderTemplate: %s\n", err)
-			return
+			return myCache, err
 		}
-	} else {
-		// we have the template in the cache.
-		log.Println("RenderTemplate: Using cached template")
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+
 	}
-
-	tmpl = tc[t]
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("RenderTemplate: %s\n", err)
-		return
-	}
-}
-
-// createTemplateCache is a function that parses template t and passes it into the cache.
-func createTemplateCache(t string) error {
-	templates := []string{
-		fmt.Sprintf("./templates/%s", t),
-		"./templates/base.layout.tmpl",
-	}
-
-	// Parse the template.
-	tmpl, err := template.ParseFiles(templates...)
-	if err != nil {
-		return err
-	}
-
-	// add template to cache.
-	tc[t] = tmpl
-
-	return nil
+	return myCache, nil
 }
